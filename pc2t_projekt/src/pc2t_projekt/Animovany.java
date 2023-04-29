e59c39b88ecbec9ef64b8f2b07c7c90bc555b1d6
pyrot <pyrot@192.168.0.87>
2023-04-29 11:40:07 +0200

    oprava

--- pc2t_projekt/src/pc2t_projekt/Animovany.java
+++ pc2t_projekt/src/pc2t_projekt/Animovany.java
@@ -1,11 +1,10 @@
 package pc2t_projekt;
 
 public class Animovany extends Film {
-	public Animovany(String nazev, String reziser, int rok, float hodnoceni, int vekDivaka) {
+	public Animovany(String nazev, String reziser, int rok, int vekDivaka) {
 		this.nazev=nazev;
 		this.reziser=reziser;
 		this.rok=rok;
-		this.hodnoceni=hodnoceni;
 		this.setVekDivaka(vekDivaka);
 	}
 	
--- pc2t_projekt/src/pc2t_projekt/Hrany.java
+++ pc2t_projekt/src/pc2t_projekt/Hrany.java
@@ -1,11 +1,10 @@
 package pc2t_projekt;
 
 public class Hrany extends Film{
-	public Hrany(String a, String b, int c, float d) {
+	public Hrany(String a, String b, int c) {
 		this.nazev=a;
 		this.reziser=b;
 		this.rok=c;
-		this.hodnoceni=d;
 	}
 	
 	public int getHranyHodnoceni() {
