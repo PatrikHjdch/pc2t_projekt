b251fe79b6293ab71cd89b1bc4db5bdc11639d45
pyrot <pyrot@192.168.0.87>
2023-04-29 12:30:45 +0200

    mm

--- pc2t_projekt/src/pc2t_projekt/Main.java
+++ pc2t_projekt/src/pc2t_projekt/Main.java
@@ -43,25 +43,26 @@ public class Main {
 		switch(volba)
 		{
 			case 1:
-				;
+				System.out.println("1 .. Pridani noveho filmu");
+				break;
 			case 2:
-				;
+				break;
 			case 3:
-				;
+				break;
 			case 4:
-				;
+				break;
 			case 5:
-				;
+				break;
 			case 6:
-				;
+				break;
 			case 7:
-				;
+				break;
 			case 8:
-				;
+				break;
 			case 9:
-				;
+				break;
 			case 10:
-				;	
+				break;	
 			case 11:
 				run = false;
 				break;
